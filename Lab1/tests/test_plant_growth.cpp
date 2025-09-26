7bd67dfdfa3abfe1444296e91c3e84d5377d02b7
nxxxk <fiydexx@gmail.com>
2025-09-26 16:43:10 +0300

    Update test_plant_growth.cpp

--- Lab1/tests/test_plant_growth.cpp
+++ Lab1/tests/test_plant_growth.cpp
@@ -2,14 +2,14 @@
 #include "../include/plant_growth.h"
 
 TEST(PlantGrowthTest, ExampleCase) {
-  EXPECT_EQ(daysToReach(100, 10, 910), 10);
+  EXPECT_EQ(daystoreach(100, 10, 910), 10);
 }
 
 TEST(PlantGrowthTest, SmallHeights) {
-  EXPECT_EQ(daysToReach(5, 1, 6), 2);
-  EXPECT_EQ(daysToReach(5, 1, 5), 1);
+  EXPECT_EQ(daystoreach(5, 1, 6), 2);
+  EXPECT_EQ(daystoreach(5, 1, 5), 1);
 }
 
 TEST(PlantGrowthTest, NoDownNeeded) {
-  EXPECT_EQ(daysToReach(10, 9, 10), 1);
+  EXPECT_EQ(daystoreach(10, 9, 10), 1);
 }
